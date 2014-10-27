a34317c680a1dcc39db8b6d01f7f4468a447fce3
moritz haarmann <mo@superflomo.com>
2014-10-27 10:21:15 +0100

    + Small improvmenet to the ocr.

--- lib/multiocr.rb
+++ lib/multiocr.rb
@@ -8,9 +8,8 @@ module MultiOCR
     def recognize(filename)
       best_confidence = 0
       text = ""
-      accepted_decline = 2
-      1.upto(10) do |t|
-        r = Recognizer.new(filename, (2.5 + (t/2.0)) * 0.1)
+      1.upto(6) do |t|
+        r = Recognizer.new(filename, (2.5 + t) * 0.1)
         
         r.run
         
@@ -18,9 +17,7 @@ module MultiOCR
           text = r.text
           best_confidence = r.average_confidence 
         elsif (best_confidence > 0 && r.average_confidence < best_confidence)
-          accepted_decline=  accepted_decline - 1 
-          
-          break if accepted_decline = 0
+
         end
         
       end
