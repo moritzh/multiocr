4e87b931727acd30010c12bc85323ad0d89a8084
moritz haarmann <mo@superflomo.com>
2014-10-27 10:03:20 +0100

    + Some changes.

--- lib/multiocr.rb
+++ lib/multiocr.rb
@@ -4,9 +4,11 @@ require 'tesseract-ocr'
 
 module MultiOCR
   class OCR
+    attr_accessor :text
     def recognize(filename)
       best_confidence = 0
       text = ""
+      accepted_decline = 2
       1.upto(10) do |t|
         r = Recognizer.new(filename, (2.5 + (t/2.0)) * 0.1)
         
@@ -14,14 +16,16 @@ module MultiOCR
         
         if (r.average_confidence > best_confidence)
           text = r.text
-          best_confidence = r.average_confidence
+          best_confidence = r.average_confidence 
         elsif (best_confidence > 0 && r.average_confidence < best_confidence)
-          break
+          accepted_decline=  accepted_decline - 1 
+          
+          break if accepted_decline = 0
         end
         
       end
       
-      puts text
+      @text = text
       
     end
   end
